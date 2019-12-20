ab5d42361010e58a4ea3d2daf0de8075ed67bb5a
Pradip Caulagi <caulagi@gmail.com>
2019-12-20 18:29:18 +0100

    Use trust-dns to resolve fortuneservice (#81)
    Relates to #71
    
    Signed-off-by: Pradip Caulagi <caulagi@gmail.com>

--- frontendservice/src/main.rs
+++ frontendservice/src/main.rs
@@ -1,7 +1,6 @@
 use std::env;
-use std::os::unix::io::AsRawFd;
-use std::os::unix::io::FromRawFd;
-use std::process::{Command, Stdio};
+
+use trust_dns_resolver::Resolver;
 use warp::Filter;
 
 pub mod fortune {
@@ -16,53 +15,21 @@ struct ServerError;
 impl warp::reject::Reject for ServerError {}
 
 /// Resolve the hostname (like fortuneservice) to an ip where
-/// the service is running. The result is used both as a string
-/// and IPV4Addr, so returning string is more generic.
-///
-/// FIXME: Use a better solution (maybe domain or trust-dns)?
-fn hostname_to_ip() -> String {
+/// the service is running.
+fn hostname_to_ip(name: &str) -> String {
+    let resolver = Resolver::from_system_conf().unwrap();
+    let response = resolver.lookup_ip(name).unwrap();
+    let address = response.iter().next().expect("no addresses returned!");
+    log::debug!("{} resolved to {:?}", name, address);
+    address.to_string()
+}
+
+async fn get_fortune() -> Result<std::boxed::Box<std::string::String>, Box<dyn std::error::Error>> {
     let service_hostname = match env::var("FORTUNE_SERVICE_HOSTNAME") {
         Ok(val) => val,
         Err(_) => panic!("Not able to find FORTUNE_SERVICE_HOSTNAME"),
     };
-    let getent_path = match env::var("GETENT_PATH") {
-        Ok(val) => val,
-        Err(_) => panic!("Not able to find GETENT_PATH"),
-    };
-    log::info!(
-        "resolving fortuneservice: {}, getent_path: {}",
-        service_hostname,
-        getent_path
-    );
-    let child = Command::new(getent_path)
-        .args(&["hosts", service_hostname.as_str()])
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("should resolve");
-    log::debug!("{:?}", child);
-    let out = match child.stdout {
-        Some(stdout) => stdout,
-        None => panic!("getent failed"),
-    };
-    log::debug!("{:?}", out);
-    let stdio = unsafe { Stdio::from_raw_fd(out.as_raw_fd()) };
-    let first_ip = Command::new("head")
-        .args(&["-1"])
-        .stdin(stdio)
-        .output()
-        .expect("2");
-    let a = String::from_utf8(first_ip.stdout).expect("first ip stdout");
-    let mut parts = a.split_whitespace();
-    let ip = match parts.next() {
-        Some(x) => x,
-        None => panic!("No ip"),
-    };
-    log::info!("{} resolved to {:?}", service_hostname, ip);
-    ip.to_string()
-}
-
-async fn get_fortune() -> Result<std::boxed::Box<std::string::String>, Box<dyn std::error::Error>> {
-    let uri = format!("http://{}:50051", hostname_to_ip());
+    let uri = format!("http://{}:50051", hostname_to_ip(&service_hostname));
     let mut client = FortuneClient::connect(uri).await?;
     let request = tonic::Request::new(FortuneRequest {});
     let response = client.get_random_fortune(request).await?;
