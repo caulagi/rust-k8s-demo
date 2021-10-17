16a98f82452e0a9f989f4921cdb4c8c638395961
Pradip Caulagi <caulagi@gmail.com>
2021-10-17 08:40:59 +0200

    Add tokio-tracing instead of logs (#261)
    Signed-off-by: Pradip Caulagi <caulagi@gmail.com>

--- frontendservice/src/main.rs
+++ frontendservice/src/main.rs
@@ -1,8 +1,11 @@
-use std::{env, io, net::SocketAddr};
+use std::{env, error::Error, io, net::SocketAddr};
 
 use axum::{handler::get, http::StatusCode, response::IntoResponse, Router};
 use tokio::net;
 use tower_http::trace::TraceLayer;
+use tracing::{debug, error, info};
+use tracing_attributes::instrument;
+
 pub mod quotation {
     tonic::include_proto!("quotation");
 }
@@ -11,10 +14,11 @@ use quotation::{quotation_client::QuotationClient, QuotationRequest};
 
 /// Resolve the hostname (like quotationservice) to an ip where
 /// the service is running.
+#[instrument]
 async fn hostname_to_ip(name: &str) -> io::Result<String> {
     for addr in net::lookup_host(name).await? {
         if addr.is_ipv4() {
-            log::debug!("{} resolved to {:?}", name, addr);
+            debug!("{} resolved to {:?}", name, addr);
             return Ok(addr.to_string());
         }
     }
@@ -38,31 +42,37 @@ async fn get_quotation() -> Result<String, Box<dyn std::error::Error>> {
     Ok(response.into_inner().message)
 }
 
+#[instrument]
 async fn handler() -> impl IntoResponse {
     match get_quotation().await {
         Ok(val) => {
-            log::debug!("Received quotation: {:?}", val);
+            debug!("Received quotation: {:?}", val);
             (StatusCode::OK, val)
         }
         Err(e) => {
-            log::error!("ERROR: {:?}", e);
+            error!("{:?}", e);
             (StatusCode::INTERNAL_SERVER_ERROR, e.to_string())
         }
     }
 }
 
+#[instrument]
 #[tokio::main]
-async fn main() {
-    pretty_env_logger::init();
+async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
+    use tracing_subscriber::EnvFilter;
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env().add_directive("async_fn=trace".parse()?))
+        .init();
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     let app = Router::new()
         .route("/", get(handler))
         .layer(TraceLayer::new_for_http());
 
-    log::info!("Frontend service starting on {:?}", addr);
+    info!("Frontend service starting on {:?}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
+    Ok(())
 }
